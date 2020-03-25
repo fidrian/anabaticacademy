2cab0d9fae52eb4bb667aa77714dbc1cc63febf6
M Fadil Fidrian <fidrianf@gmail.com>
2020-03-25 16:14:45 +0700

    update shop

--- services/anabatic_academy.sql
+++ services/anabatic_academy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: remotemysql.com
--- Generation Time: Mar 25, 2020 at 01:30 AM
+-- Generation Time: Mar 25, 2020 at 08:54 AM
 -- Server version: 8.0.13-4
 -- PHP Version: 7.0.33-0ubuntu0.16.04.3
 
@@ -212,7 +212,9 @@ INSERT INTO `user` (`user_id`, `username`, `password`, `role`, `email`, `nama_le
 (4, 'euler', 'leonhard', 'user', NULL, NULL),
 (5, 'faraday', 'michael', 'user', NULL, NULL),
 (6, 'fidrian', 'qwerty123', 'user', 'fidrian123@gmail.com', 'M. Fadil Fidrian'),
-(7, 'julian', 'julian', 'user', 'julian@gmail.com', 'julian');
+(7, 'julian', 'julian', 'user', 'julian@gmail.com', 'julian'),
+(8, 'binar', 'qwerty1234.', 'user', 'binar@gmail.com', 'binariyantoaji'),
+(9, 'kartiko', '123456', 'user', 'kartiko@gmail.com', 'Kartiko Pramudito');
 
 --
 -- Indexes for dumped tables
@@ -292,7 +294,7 @@ ALTER TABLE `unggah`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- Constraints for dumped tables
 --
